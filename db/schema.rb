73508a13d6b3f401135806dbcd0441283789714f
Orlando Montoya <omb0320@gmail.com>
2016-12-18 21:11:08 -0500

    Rename attribute release_at by released_at

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161218081750) do
+ActiveRecord::Schema.define(version: 20161219020330) do
 
   create_table "albums", force: :cascade do |t|
     t.string   "name"
     t.string   "image_url"
-    t.string   "release_at"
+    t.string   "released_at"
     t.integer  "artist_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.index ["artist_id"], name: "index_albums_on_artist_id"
   end
 
