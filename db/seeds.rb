133b9460464101cbda2518999246ad95338c8330
Orlando Montoya <omb0320@gmail.com>
2016-12-20 23:49:22 -0500

    Agrega cambios en la forma de mostrar artistas

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
 Artist.create(name: "J Álvarez", image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-Mq7TJq-h3J8e2q7Y9ihrxheNJ2HYrfi_wpZxn47eou-Zyxc0")
 
 Artist.create(name: "Cheka", image_url: "http://www.reggaetonline.net//images/artists/cheka.jpg")
